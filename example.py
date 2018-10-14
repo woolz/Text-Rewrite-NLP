e3defa28192accb5e85bdbadf54dc57eb96a6fd4
Thiago Cassimiro <43118264+woolz@users.noreply.github.com>
2018-10-13 22:28:51 -0300

    Update example.py

--- example.py
+++ example.py
@@ -1,8 +1,7 @@
 from text_rewrite import TextRewrite
 
 
-if __name__ == "__main__":
-    sentences = ['My machine is so bad and dramatic', 'I have one dog and two cars', 'This season is so weak.']
-    for sentence in sentences:
-      new_sentence = TextRewrite(sentence).work() 
-      print(sentence + " -> " + new_sentence)
+sentences = ['My machine is so bad and dramatic', 'I have one dog and two cars', 'This season is so weak.']
+for sentence in sentences:
+    new_sentence = TextRewrite(sentence).work() 
+    print(sentence + " -> " + new_sentence)
